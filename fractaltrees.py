ac4c01f2459728a519f015240f55c7650286a2a9
koopa35 <connorpowell111@gmail.com>
2023-07-17 01:07:37 -0400

    Finished

--- fractaltrees.py
+++ fractaltrees.py
@@ -1,28 +1,48 @@
 import turtle
 
-
 #Turtle Set Up with Window Set Up
 window = turtle.Screen()
 window.bgcolor("white")
 window.title("Fractal Tree")
+window.colormode(255)
 pen = turtle.Turtle()
+pen.speed(0)
 
-
-
+#Branches variable is how many recursive branches deep the tree goes
+BRANCHES = int(input("Enter a Number of Branches: "))
 def branch(ITTERATION):
-    pen.down()
-    pen.forward(100/ITTERATION)
-    pen.up()
+    if ITTERATION < BRANCHES:
+        pen.color(0, 0, 0 + (255//BRANCHES)*ITTERATION)
+        #Left Branch
+        pen.down()
+        POS = pen.position()
+        pen.left(25)
+        HEAD = pen.heading()
+        pen.forward(100/ITTERATION)
+        branch(ITTERATION + 1)
+        
+        #Reset
+        pen.up()
+        pen.setposition(POS)
+        pen.setheading(HEAD)
+        pen.color(0, 0, 0 + (255//BRANCHES)*ITTERATION)
 
-
-
+        
+        #Right Branch
+        pen.right(50)
+        pen.down()
+        pen.forward(100/ITTERATION)
+        branch(ITTERATION + 1)
 
 #Puts the pen in the starting positon
-pen.speed(1)
+pen.color(0, 0, (255//BRANCHES))
 pen.up()
 pen.left(90)
-pen.goto(0,-340)
+pen.setposition(0,-500)
+pen.down()
+pen.forward(200)
 
-branch(1)
+ITTERATIONS = 1
+branch(ITTERATIONS)
 
 turtle.done()
